f908b7c30cc520f08ee57f8d9d4a3bb4d39a7df9
gechetspr <serhii.chepela@spryker.com>
2019-11-07 18:29:35 +0200

    TE-4417 fixed tests for phpstan 8

--- src/Silex/WebTestCase.php
+++ src/Silex/WebTestCase.php
@@ -35,7 +35,7 @@ abstract class WebTestCase extends TestCase
      * Note: Child classes that define a setUp method must call
      * parent::setUp().
      */
-    public function setUp()
+    public function setUp(): void
     {
         $this->app = $this->createApplication();
     }
