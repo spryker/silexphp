44a6be99f8c315742b7a7b172fc2473506dff526
gechetspr <serhii.chepela@spryker.com>
2019-11-07 18:04:40 +0200

    TE-4417 updated tests base class

--- tests/Silex/Tests/ControllerCollectionTest.php
+++ tests/Silex/Tests/ControllerCollectionTest.php
@@ -11,6 +11,7 @@
 
 namespace Silex\Tests;
 
+use PHPUnit\Framework\TestCase;
 use Silex\Controller;
 use Silex\ControllerCollection;
 use Silex\Exception\ControllerFrozenException;
@@ -21,7 +22,7 @@ use Silex\Route;
  *
  * @author Igor Wiedler <igor@wiedler.ch>
  */
-class ControllerCollectionTest extends \PHPUnit_Framework_TestCase
+class ControllerCollectionTest extends TestCase
 {
     public function testGetRouteCollectionWithNoRoutes()
     {
