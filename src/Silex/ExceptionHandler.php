af956ffa370d5ba45d728f6b71c5c13a9d0fff36
René Klatt <rene.klatt@spryker.com>
2020-08-13 11:25:28 +0000

    TE-7001 Fixed tests.

--- src/Silex/ExceptionHandler.php
+++ src/Silex/ExceptionHandler.php
@@ -12,6 +12,7 @@
 namespace Silex;
 
 use Symfony\Component\Debug\ExceptionHandler as DebugExceptionHandler;
+use Symfony\Component\Debug\Exception\FlattenException;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
@@ -49,17 +50,13 @@ class ExceptionHandler implements EventSubscriberInterface
 
         $handler = new DebugExceptionHandler($this->debug);
 
-        if (method_exists($handler, 'getHtml')) {
-            $exception = $event->getThrowable();
-            if (!$exception instanceof FlattenException) {
-                $exception = $this->flattenException($exception);
-            }
-
-            $response = Response::create($handler->getHtml($exception), $exception->getStatusCode(), $exception->getHeaders())->setCharset(ini_get('default_charset'));
-        } else {
-            // BC with Symfony < 2.8
-            $response = $handler->createResponse($event->getThrowable());
+        $exception = $event->getThrowable();
+        if (!$exception instanceof FlattenException) {
+            $exception = $this->flattenException($exception);
         }
+
+        $response = new Response($handler->getHtml($exception), $exception->getStatusCode(), $exception->getHeaders());
+        $response->setCharset(ini_get('default_charset'));
 
         $event->setResponse($response);
     }
